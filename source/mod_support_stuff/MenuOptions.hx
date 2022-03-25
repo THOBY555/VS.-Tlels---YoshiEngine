00bf2be00f3157918a4354418316b9034519618c
REFOOL <71563419+THOBY555@users.noreply.github.com>
2022-03-25 11:11:30 +0300

    so i can set the menu shit pos

--- source/mod_support_stuff/MenuOptions.hx
+++ source/mod_support_stuff/MenuOptions.hx
@@ -11,22 +11,32 @@ typedef MenuOption = {
 	var selected:String; // 'story mode white'
 	@:optional var selectedFPS:Null<Int>; // 24
     @:optional var direct:Null<Bool>; // true if it does the action directly like the "donate" button
+	public var x:Int;
+    public var y:Int;
 }
 
 class MenuOptions {
     public var members:Array<MenuOption> = [];
+
+    public var xShit:Int;
+    public var yShit:Int;
+
     public function new(?anims:Array<MenuOption>) {
         if (anims != null) members = anims;
     }
 
-    public function add(name:String, onSelect:Void->Void, frames:FlxAtlasFrames, idle:String, selected:String):MenuOption {
+    public function add(name:String, onSelect:Void->Void, frames:FlxAtlasFrames, idle:String, selected:String, x:Int, y:Int):MenuOption {
         var obj:MenuOption = {
             name: name,
             onSelect: onSelect,
             frames: frames,
             idle: idle,
-            selected: selected
+            selected: selected,
+            x: x,
+            y: y
         };
+        xShit = x;
+        yShit = y;
         members.push(obj);
         return obj;
     }
@@ -48,16 +58,23 @@ class MenuOptions {
         members.insert(index, option);
     }
 
-    public function insert(index:Int, name:String, onSelect:Void->Void, frames:FlxAtlasFrames, idle:String, selected:String) {
+    public function insert(index:Int, name:String, onSelect:Void->Void, frames:FlxAtlasFrames, idle:String, selected:String, x:Int, y:Int) {
         members.insert(index, {
             name: name,
             onSelect: onSelect,
             frames: frames,
             idle: idle,
-            selected: selected
+            selected: selected,
+            x: x,
+            y: y
         });
+        xShit = x;
+        yShit = y;
     }
 
     public var length(get, null):Int;
-    private function get_length() {return members.length;};
+    private function get_length() {
+        return members.length;
+    };
+
 }
